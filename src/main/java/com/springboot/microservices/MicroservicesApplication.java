4101ebd7c72961ca1907b97d70dcd02c6eae2d52
Prit Lakhani <pritlakhani@Prits-MacBook-Pro.local>
2023-12-27 14:00:25 -0800

    microservices setup

---
+++ src/main/java/com/springboot/microservices/MicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesApplication.class, args);
+    }
+
+}
