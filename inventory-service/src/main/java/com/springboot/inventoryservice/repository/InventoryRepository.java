3c492f7cc9c172c450f520a678067671e6451966
Prit Lakhani <pritlakhani@Prits-MacBook-Pro.local>
2023-12-27 20:36:14 -0800

    API for chcking inventory

--- inventory-service/src/main/java/com/springboot/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/springboot/inventoryservice/repository/InventoryRepository.java
@@ -3,5 +3,8 @@ package com.springboot.inventoryservice.repository;
 import com.springboot.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
