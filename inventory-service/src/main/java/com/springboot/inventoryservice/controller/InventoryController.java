ee7e1369f27dab914741ec3d2a2c55dc8b4a2a82
Prit Lakhani <pritlakhani@Prits-MacBook-Pro.local>
2023-12-29 14:13:02 -0800

    calling invetnroy service before placing an order to check the stock

--- inventory-service/src/main/java/com/springboot/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/springboot/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.springboot.inventoryservice.controller;
 
+import com.springboot.inventoryservice.dto.InventoryResponse;
 import com.springboot.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -11,9 +14,9 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
 
     private final InventoryService inventoryService;
-    @GetMapping("/{sku-code}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
 
         return inventoryService.isInStock(skuCode);
 
--- inventory-service/src/main/java/com/springboot/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/springboot/inventoryservice/service/InventoryService.java
@@ -1,19 +1,32 @@
 package com.springboot.inventoryservice.service;
 
+import com.springboot.inventoryservice.dto.InventoryResponse;
 import com.springboot.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Checking Inventory...");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }
 
